02d719d2a65d7006488327f97f1cfa33e6bb27d3
Freddie Vargus <fvargus4@bu.edu>
2017-05-17 15:30:40 -0400

    MAINT: Remove code that doesn't work with the new yahoo API

--- zipline/data/__init__.py
+++ zipline/data/__init__.py
@@ -1,14 +1,12 @@
 from . import loader
 from .loader import (
     load_from_yahoo,
-    load_bars_from_yahoo,
     load_prices_from_csv,
     load_prices_from_csv_folder,
 )
 
 
 __all__ = [
-    'load_bars_from_yahoo',
     'load_from_yahoo',
     'load_prices_from_csv',
     'load_prices_from_csv_folder',
